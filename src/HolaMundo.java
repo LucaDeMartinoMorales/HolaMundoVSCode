b9594128a5e29799730e03cc0d3e27501e66bb01
LucaDeMartino <lmarmor299@g.educaand.es>
2025-10-31 09:15:08 +0100

    Segunda version

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo!");
+        System.out.println("Hola mundo!");
+        System.out.println("Hola mundo!");
     }
 }
